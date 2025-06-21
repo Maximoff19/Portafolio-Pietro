13af2ce029e737660fd3350710500f9433cf8bbe
pietro <pietroxz19@gmail.com>
2025-06-21 17:02:25 -0500

    main

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://maximoff19.github.io/Portafolio_v0.02/',
+  base: 'https://maximoff19.github.io/github-pages'
+  // ',
 })
